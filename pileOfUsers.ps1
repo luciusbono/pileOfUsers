6023441bf147e1b8770483afd4a7a725bdc70599
Lucius Bono <luciusbono@gmail.com>
2014-09-26 17:59:47 -0700

    Initial script upload

---
+++ pileOfUsers.ps1
@@ -0,0 +1,23 @@
+﻿param($inputfile = [Environment]::GetFolderPath("Desktop")+'\'+'users.csv', $outputBad = [Environment]::GetFolderPath("Desktop")+'\'+'outputBad.csv', 
+$outputGood = [Environment]::GetFolderPath("Desktop")+'\'+'outputGood.csv')
+
+$input = Import-CSV $inputfile
+
+New-Item $outputBad -type file -force
+
+Write-Host `n `n "Exporting users to: " `n $outputBad `n $outputGood
+
+$(foreach($_.samaccountname in $input)
+{
+	$chkusr = Get-QADUser -name $_.samaccountname
+	if($chkusr)
+	{
+		$chkusr | select displayname,email
+	}
+	else
+		{
+			Add-Content $outputBad $_.samaccountname
+		}
+}) | Export-Csv $outputGood -NoTypeInformation
+
+Write-Host `n `n "Done!"
